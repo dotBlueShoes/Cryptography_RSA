2ec37efa31380750395d81bdf7cd516faf378a50
dotBlueShoes <matthewstrumillo@gmail.com>
2023-07-03 11:28:18 +0200

    Moved IO

---
+++ RSA-Java/src/main/java/crypto/FileIO.java
@@ -0,0 +1,37 @@
+package crypto;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class FileIO {
+
+	public static byte[] ReadFileToBytes(File file) {
+		try {
+
+			byte[] fileContents = Files.readAllBytes(file.toPath());
+
+			if (fileContents.length == 0) {
+				throw new IOException();
+			}
+
+			return fileContents;
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+			// Handle file empty and such errors...
+			// return new byte[]{ 0 };
+		}
+	}
+
+	public static void WriteBytesToFile(String filePath, byte[] data) {
+		try (FileOutputStream fos = new FileOutputStream(filePath)) {
+			fos.write(data);
+			//fos.close(); There is no more need for this line since you had created the instance of "fos" inside the try. And this will automatically close the OutputStream
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
--- RSA-Java/src/main/java/crypto/Main.java
+++ RSA-Java/src/main/java/crypto/Main.java
@@ -10,59 +10,23 @@ package crypto;
 // 7. Tests?
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
 
 public class Main {
+
+	public static String output = "D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\o1.txt";
+	public static String t0 = "D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\t0.txt";
+	public static String t1 = "D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\t1.txt";
+	public static String t2 = "D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\t2.txt";
+	public static String t3 = "D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\t3.txt";
+	public static String t4 = "D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\t4.txt";
+
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 
-		//File file = new File("D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\t0.txt");
-		//File file = new File("D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\t1.txt");
-		//File file = new File("D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\t2.txt");
-		//File file = new File("D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\t3.txt");
-		File file = new File("D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\t4.txt");
-		//File output = new File("D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\o1.txt");
-
-		byte[] fileContent;
-
-		try { // READING
-			fileContent = Files.readAllBytes(file.toPath());
-
-			if (fileContent.length == 0) {
-				throw new IOException();
-			}
-
-			System.out.print("length: " + fileContent.length);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-			// Handle file empty and such errors...
-		}
-
-		//try { // WRITING
-		//	FileWriter myWriter = new FileWriter("D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\o1.txt");
-		//	myWriter.write(fileContent);
-		//} catch (IOException e) {
-		//	throw new RuntimeException(e);
-		//}
-
-		try (FileOutputStream fos = new FileOutputStream("D:\\Storage\\Projects\\School\\cryptography\\Cryptography_RSA\\data\\o1.txt")) {
-			fos.write(fileContent);
-			//fos.close(); There is no more need for this line since you had created the instance of "fos" inside the try. And this will automatically close the OutputStream
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
+		File file = new File(t4);
+		byte[] data = FileIO.ReadFileToBytes(file);
+		FileIO.WriteBytesToFile(output, data);
 
 	}
 
-	public void ReadFileToBytes() {
-
-	}
-
-	public void WriteBytesToFile() {
-
-	}
 }
