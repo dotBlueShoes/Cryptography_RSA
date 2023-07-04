320afb376767d8cfbf3377da92f9846580ed1b1b
dotBlueShoes <matthewstrumillo@gmail.com>
2023-07-04 16:21:48 +0200

    Control connection

--- RSA-Java/View/src/main/java/crypto/view/MainController.java
+++ RSA-Java/View/src/main/java/crypto/view/MainController.java
@@ -1,14 +1,39 @@
 package crypto.view;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class MainController {
 	@FXML
-	private Label welcomeText;
+	private TextArea textAreaP, textAreaQ, textAreaN, textAreaE, textAreaD, textAreaTextInput, textAreaTextOutput;
 
 	@FXML
-	protected void onHelloButtonClick() {
-		welcomeText.setText("Welcome to JavaFX Application!");
+	protected void onFileEncryptClick() {
+		textAreaP.setText("A!");
+	}
+
+	@FXML
+	protected void onFileDecryptClick() {
+		textAreaP.setText("B!");
+	}
+
+	@FXML
+	protected void onFileSelectInputClick() {
+		textAreaP.setText("C!");
+	}
+
+	@FXML
+	protected void onFileSelectOutputClick() {
+		textAreaP.setText("D!");
+	}
+
+	@FXML
+	protected void onTextEncryptClick() {
+		textAreaP.setText("E!");
+	}
+
+	@FXML
+	protected void onTextDecryptClick() {
+		textAreaP.setText("F!");
 	}
 }
