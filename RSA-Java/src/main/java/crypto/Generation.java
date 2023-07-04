aadb9cea019ed217e6a082e129e0510a813e02ca
Westesc <mikolaj.kisiel@o2.pl>
2023-07-04 17:37:00 +0200

    RSA
    We have to check if everything is correct

--- RSA-Java/src/main/java/crypto/Generation.java
+++ RSA-Java/src/main/java/crypto/Generation.java
@@ -3,13 +3,25 @@ package crypto;
 import java.math.BigInteger;
 
 public class Generation {
+	private static boolean isPrimeNumber(BigInteger i){
+		for( BigInteger j = BigInteger.TWO; j.compareTo(i.divide(BigInteger.TWO)) < 0; j.add(BigInteger.ONE)) {
+			if(i.remainder(j).equals(BigInteger.ZERO))
+				return false;
+		}
+		return true;
+	}
 
-	public static BigInteger generateE(BigInteger a, BigInteger b) {
+	public static BigInteger generateE(BigInteger a) {
+		for (BigInteger i = a.subtract(BigInteger.ONE); i.compareTo(BigInteger.ONE) > 0; i.subtract(BigInteger.ONE))
+			if(isPrimeNumber(i) && (a.remainder(i)).equals(BigInteger.ZERO))
+				return i;
 		return BigInteger.ONE;
 	}
 
 	public static BigInteger generateD(BigInteger a, BigInteger b) {
-		return BigInteger.ONE;
+		for(BigInteger i = BigInteger.ONE; true; i.add(BigInteger.ONE))
+			if((((b.multiply(i)).add(BigInteger.ONE)).remainder(a)).equals(BigInteger.ZERO))
+				return ((b.multiply(i)).add(BigInteger.ONE)).divide(a);
 	}
 
 }
--- RSA-Java/src/main/java/crypto/RSA.java
+++ RSA-Java/src/main/java/crypto/RSA.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 
 public class RSA {
 
+
 	// Bytes to BigInt
 	// BigInt to Bytes
 	// encryption, decryption tests
@@ -13,7 +14,7 @@ public class RSA {
 	public static void initialize(final BigInteger p, final BigInteger q) {
 		n = p.multiply(q);
 		phi = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
-		e = Generation.generateE(phi, n);
+		e = Generation.generateE(phi);
 		d = Generation.generateD(e, phi);
 	}
 
@@ -24,5 +25,20 @@ public class RSA {
 	public static void decrypt() {
 
 	}
+     public static BigInteger encrypt (BigInteger m) {
+         return m.modPow(e,n);
+     }
+
+    public static BigInteger dencrypt (BigInteger c) {
+        return c.modPow(d,n);
+    }
+
+    public BigInteger getE() {
+        return e;
+    }
+
+    public BigInteger getD() {
+        return d;
+    }
 
 }
